f0e42a594e6aab7a659d64518374d92803b290b6
Katja Seltmann <seltmann@169-231-172-219.wireless.ucsb.edu>
2025-06-25 16:02:16 -0700

    analysis

--- clean-data-mtepidus.R
+++ clean-data-mtepidus.R
@@ -57,24 +57,3 @@ bee_data <- bee_data %>%
 # View result
 bee_data %>%
   select(Date, Time_Entered_fixed, Time_Entered_dt, Time_Exited_fixed, Time_Exited_dt, Duration_minutes)
-
-
-#number females per nest
-bee_counts <- bee_data %>%
-  filter(!is.na(Bee_color) & !is.na(Nest_Number)) %>%
-  group_by(Nest_Number) %>%
-  summarise(
-    num_females = n_distinct(Bee_color),
-    .groups = "drop"
-  )
-
-# View the result
-bee_counts
-
-
-total_nests <- nrow(bee_counts)
-nests_with_multiple_females <- bee_counts %>%
-  filter(num_females > 1) %>%
-  nrow()
-
-cat("Nests with >1 female:", nests_with_multiple_females, "out of", total_nests, "total nests\n")
